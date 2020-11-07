9ec752402a59338561a177bf1c14c25d619c97cb
kuramochi <kura19971106@gmail.com>
2020-11-07 12:18:14 +0900

    48329, only path optimization

--- src/Answer.cpp
+++ src/Answer.cpp
@@ -17,11 +17,14 @@ namespace hpc {
 typedef std::vector<Vector2> Ver;
 
 const double INF = 1e50;
-// rabbit : 0, others : 1 - 20
+/// rabbit : 0, others : 1 - 20
 const int MAX_V = 21;
 
-// ウサギ&巻物 間の速さをジャンプ力を1.0とした場合の移動時間
+/// ウサギ&巻物 間の速さをジャンプ力を1.0とした場合の移動時間
 double distance[25][25];
+
+/// 巻物の取る順番
+std::vector<int> targets;
 
 double dist(Vector2 v1, Vector2 v2){
     double diff_y = v1.y - v2.y;
@@ -29,7 +32,7 @@ double dist(Vector2 v1, Vector2 v2){
     return std::sqrt(diff_y*diff_y + diff_x*diff_x);
 }
 
-// 各点間をINFに初期化
+/// 各点間をINFに初期化
 void distance_init(const Stage& aStage){
     int size = aStage.scrolls().count();
     for(int i = 0; i < size; i++)
@@ -38,7 +41,7 @@ void distance_init(const Stage& aStage){
             else distance[i][j] = INF;
 }
 
-// 全点間距離を求める。()
+/// 全点間距離を求める。()
 void warshall_floyd(Ver vertices){
     int size = vertices.size();
     for(int i = 0; i < size; i++)
@@ -52,7 +55,7 @@ void warshall_floyd(Ver vertices){
                 distance[i][j] = std::min(distance[i][j], distance[i][k] + distance[k][j]);
 }
 
-// 直線距離で全点間のdistanceを更新
+/// 直線距離で全点間のdistanceを更新
 void calc_distance_greedy(Ver vertices){
     int size = vertices.size();
     for(int i = 0; i < size; i++){
@@ -64,11 +67,11 @@ void calc_distance_greedy(Ver vertices){
     }
 }
 
-// 最短経路での移動先の点
+/// 最短経路での移動先の点
 int next_vertex[1 << MAX_V][MAX_V];
-// 計算に使用 TODO:change
+/// 計算に使用 TODO:change
 double DP[1 << MAX_V][MAX_V];
-// DP配列の初期化
+/// DP配列の初期化
 void sales_man_init(int size){
     for(int i = 0; i < (1 << size); i++){
         for(int j = 0; j < size; j++){
@@ -78,8 +81,8 @@ void sales_man_init(int size){
     }
 }
 
-// すべての頂点を一度ずつめぐり帰ってくる経路のうち、重みの総和の最小値を求める。
-// O(2**n n**2)
+/// すべての頂点を一度ずつめぐり帰ってくる経路のうち、重みの総和の最小値を求める。
+/// O(2**n n**2)
 double sales_man(int S, int v, int size){
     if(DP[S][v] >= 0){
         return DP[S][v];
@@ -102,10 +105,30 @@ double sales_man(int S, int v, int size){
     return DP[S][v] = res;
 }
 
+/// vertice配列の初期化
 void vertices_init(Ver& vertices, const Stage& aStage){
     vertices.push_back(aStage.rabbit().pos());
     for(auto scroll : aStage.scrolls())
         vertices.push_back(scroll.pos());
+}
+
+/// next_vertex配列からtargets配列を生成
+void target_sequence(){
+    int v = 0;
+    int S = 0;
+    while(1){
+        S |= 1 << v;
+        v = next_vertex[S][v];
+        if(v == -1) break;
+        /// v=0はRabbitであるため。
+        targets.push_back(v-1);
+    }
+
+    //printf("%d : ", (int)targets.size());
+    //for(auto idx : targets){
+    //    printf("%d ", idx);
+    //}
+    //printf("\n");
 }
 
 //------------------------------------------------------------------------------
@@ -136,16 +159,8 @@ void Answer::initialize(const Stage& aStage)
 
     sales_man_init(vertices.size());
     sales_man(1 << 0, 0, vertices.size());
-    //TODO : make function
-    int v = 0;
-    int S = 0;
-    while(1){
-        if(v == -1) break;
-        printf("%d ", v);
-        S |= 1 << v;
-        v = next_vertex[S][v];
-    }
-    printf("\n");
+
+    target_sequence();
 }
 
 //------------------------------------------------------------------------------
@@ -156,8 +171,17 @@ void Answer::initialize(const Stage& aStage)
 Vector2 Answer::getTargetPos(const Stage& aStage)
 {
     auto pos = aStage.rabbit().pos();
+/*
     for(auto scroll : aStage.scrolls()) {
         // まだ手に入れていない巻物を探して、そこに向かって飛ぶ
+        if (!scroll.isGotten()) {
+            return scroll.pos();
+        }
+    }
+*/
+    auto scrolls = aStage.scrolls();
+    for(int idx : targets){
+        auto scroll = scrolls[idx];
         if (!scroll.isGotten()) {
             return scroll.pos();
         }
@@ -172,6 +196,7 @@ Vector2 Answer::getTargetPos(const Stage& aStage)
 /// @param aStage 現在のステージ
 void Answer::finalize(const Stage& aStage)
 {
+    targets.clear();
 }
 
 } // namespace
